811aa98d2bdd1b6eafd74378c2de553a0f86001d
Helene <hcblackmer@gmail.com>
2022-01-19 15:17:20 -0800

    Added spinner component to render when fetching data from API.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Spinner } from 'reactstrap';
 import Picture from '../Picture/Picture';
 import axios from 'axios';
 import { API_KEY } from '../key';
@@ -10,21 +10,26 @@ const Main = () => {
     const [picture, setPicture] = useState('');
     const [title, setTitle] = useState('');
     const [explanation, setExplanation] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         getPicture();
     }, []);
 
-    const getPicture = () => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    const getPicture = async () => {
+        try {
+            await axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
             .then(res => {
                 console.log(res);
                 setPicture(res.data.hdurl);
                 setTitle(res.data.title);
                 setExplanation(res.data.explanation);
-            }).catch(err => {
-                console.log(err);
             });
+            setLoading(true);
+        } catch (e) {
+            console.log(e);
+        }
     }
     return (
         <Container className="main mx-auto">
@@ -32,6 +37,7 @@ const Main = () => {
                 picture={picture}
                 title={title}
                 explanation={explanation}
+                loading={loading}
             />
         </Container>
     );
--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,42 +1,47 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
     Card, CardImg, CardText, CardBody, CardLink,
-    CardTitle, CardSubtitle
+    CardTitle, CardSubtitle, Spinner
 } from 'reactstrap';
 
 import './Picture.css';
 
-const Picture = ({ picture, title, explanation }) => {
+const Picture = ({ picture, title, explanation, loading }) => {
     return (
         <Card className="card text-white bg-black">
-            <CardBody>
-                <CardTitle tag="h5">
-                    {title}
-                </CardTitle>
-                <CardSubtitle
-                    className="mb-2 text-muted"
-                    tag="h6"
-                >
-                    {explanation}
-                </CardSubtitle>
-            </CardBody>
-            <CardImg
-                className="card-image"
-                alt={explanation}
-                src={picture}
-                width="50%"
-            />
-            <CardBody>
-                <CardText>
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                </CardText>
-                <CardLink href="#">
-                    Card Link
-                </CardLink>
-                <CardLink href="#">
-                    Another Link
-                </CardLink>
-            </CardBody>
+            {loading ?
+                <Fragment>
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            {title}
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            {explanation}
+                        </CardSubtitle>
+                    </CardBody>
+                    <CardImg
+                        className="card-image"
+                        alt={explanation}
+                        src={picture}
+                        width="50%"
+                    />
+                    <CardBody className="mx-auto">
+                        <CardLink href="#">
+                            Like
+                        </CardLink>
+                        <CardLink href="#">
+                            Share
+                        </CardLink>
+                    </CardBody>
+                </Fragment>
+                :
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            }
         </Card>
     );
 }
